dd73c8ddfe53a4c4adddc6cdf07f73c154ef4956
AlexMubarakshin <muban96@mail.ru>
2019-02-06 17:53:09 +0300

    Minor refactor

--- src/components/visuals/visuals.tsx
+++ src/components/visuals/visuals.tsx
@@ -9,6 +9,8 @@ export class Visual extends React.Component<IVisualsProps> {
     private canvasRef: HTMLCanvasElement;
     private canvasCtx: CanvasRenderingContext2D;
 
+    private CIRCLE_RADIUS = 100;
+
     componentDidMount() {
         const width = document.documentElement.clientWidth;
         const height = document.documentElement.clientHeight;
@@ -28,59 +30,71 @@ export class Visual extends React.Component<IVisualsProps> {
         this.canvasRef.height = document.documentElement.clientHeight;
     }
 
-
     animate = () => {
         requestAnimationFrame(this.animate);
 
         const bufferLength = this.props.analyser!.frequencyBinCount;
 
         const ctx = this.canvasCtx;
-        const canvas = this.canvasRef;
 
-        let WIDTH = canvas.width;
-        let HEIGHT = canvas.height;
-
-        let barHeight;
-        
-        let cx = WIDTH / 2;
-        let cy = HEIGHT / 2;
-        let rectHeight = 15;
         let rotation = 0;
-        
-        const rectRadius = 100;
-        const rectLength = rectRadius * 2 * Math.PI;
+
+        const rectLength = this.CIRCLE_RADIUS * 2 * Math.PI;
         let barWidth = (rectLength / bufferLength);
-        
+
         this.props.analyser!.getByteFrequencyData(this.props.dataArray!);
-        
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        
-        ctx.fillStyle = "black";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        
-        ctx.fillStyle = "#fff";
-        ctx.beginPath();
-        ctx.arc(cx, cy, rectRadius, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.fill();
-        ctx.save();
-        
-        ctx.translate(cx, cy);
+
+        this.clearBackground();
+        this.drawCircle();
 
         for (let i = 0; i < bufferLength; i++) {
-            barHeight = this.props.dataArray![i];
-            let r = barHeight + (25 * (i/bufferLength));
-            let g = 250 * (i/bufferLength);
-            let b = 50;
+            const barHeight = this.props.dataArray![i];
 
-            ctx.rotate(rotation);
-            // ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-            ctx.fillStyle = "#fff";
-            ctx.fillRect(-rectHeight / 2 + 150, -barWidth / 2, barHeight, barWidth);
+            this.drawBar(rotation, barHeight, barWidth);
+
             rotation = (rotation / (bufferLength + barWidth)) % 360 + 1;
         }
 
         ctx.restore();
+    }
+
+    private clearBackground = () => {
+        const canvas = this.canvasRef;
+        const ctx = this.canvasCtx;
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+    private drawCircle = () => {
+        const canvas = this.canvasRef;
+        const ctx = this.canvasCtx;
+
+        let WIDTH = canvas.width;
+        let HEIGHT = canvas.height;
+
+        let cx = WIDTH / 2;
+        let cy = HEIGHT / 2;
+
+        ctx.beginPath();
+        ctx.arc(cx, cy, this.CIRCLE_RADIUS, 0, Math.PI * 2);
+        ctx.lineWidth = 10;
+        ctx.strokeStyle = "#fff";
+        ctx.closePath();
+        ctx.stroke();
+        ctx.save();
+
+        ctx.translate(cx, cy);
+    }
+
+    private drawBar = (rotation: number, barHeight: number, barWidth: number) => {
+        const ctx = this.canvasCtx;
+
+        ctx.rotate(rotation);
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(150, -barWidth / 2, barHeight, barWidth);
     }
 
     render() {
