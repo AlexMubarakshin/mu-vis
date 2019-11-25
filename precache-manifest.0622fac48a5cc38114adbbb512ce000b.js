a44c86bb8b2c2ffb8ee836edaf5a3f207e32036c
Alex Mubarakshin <muban96@mail.ru>
2019-11-25 13:49:13 +0300

    Updates

---
+++ precache-manifest.0622fac48a5cc38114adbbb512ce000b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bd3084b43968b14eb60885dc6ea979",
+    "url": "/mu-vis/index.html"
+  },
+  {
+    "revision": "e1dd379b7833ec8788c8",
+    "url": "/mu-vis/static/css/main.5bfed551.chunk.css"
+  },
+  {
+    "revision": "c9bed8cebb1cead8f8ec",
+    "url": "/mu-vis/static/js/2.5cabfff4.chunk.js"
+  },
+  {
+    "revision": "e1dd379b7833ec8788c8",
+    "url": "/mu-vis/static/js/main.42ae62ff.chunk.js"
+  },
+  {
+    "revision": "d8253c7af072aa493f08",
+    "url": "/mu-vis/static/js/runtime-main.9e9205ce.js"
+  }
+]);
